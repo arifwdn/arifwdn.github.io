d864d09091307ad0b72eaaf44d40c78d0fd18a0f
Muhammad Arif Wardana <73418276+arifwdn@users.noreply.github.com>
2022-03-07 12:30:09 +0800

    Create Script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+var kesempatan = 3;
+//Pilihan Comp
+var comp = Math.floor(Math.random()*(10-1)+1);
+
+while(kesempatan >= 1){
+    // Pilihan Player
+    var p = prompt("Tebak Angka 1-10 : ");
+    //Rule
+    var hasil = '';
+
+	if(p==comp){
+		hasil = 'BENAR! \n Pilihan anda adalah ' + p + ' dan Pilihan Komputer adalah ' + comp;
+        kesempatan = 1;
+	}else{
+		var sisa = kesempatan-1;
+		hasil = (p<comp) ? 'SALAH! Terlalu rendah! \n Sisa Kesempatan = ' + sisa : 'SALAH! Terlalu tinggi! \n Sisa Kesempatan = ' + sisa; 
+	}
+    
+    alert("TEBAKAN ANDA " + hasil);
+    kesempatan--;
+}
+
+alert("Game Selesai, Terima Kasih!");
